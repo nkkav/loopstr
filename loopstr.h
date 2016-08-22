45c6a1a4c662680f1a1c537485444c57749dc942
Nikolaos Kavvadias <kavvadias@silexica.com>
2016-08-22 08:48:55 +0200

    Update for 2016

--- loopstr.h
+++ loopstr.h
@@ -1,17 +1,11 @@
 /* file "loopstr/loopstr.h" */
 /*
  *     Copyright (c) 2004, 2005, 2006, 2007, 2008, 2009, 2010,
- *                   2011, 2012, 2013, 2014 Nikolaos Kavvadias
+ *                   2011, 2012, 2013, 2014, 2015, 2016 Nikolaos Kavvadias
  *
  *     This software was written by Nikolaos Kavvadias, Ph.D. candidate
  *     at the Physics Department, Aristotle University of Thessaloniki,
  *     Greece (at the time).
- *
- *     Current affiliation:
- *     Dr. Nikolaos Kavvadias
- *     Independent Consultant -- Research Scientist
- *     Kornarou 12 Rd., Nea Ampliani,
- *     35100 Lamia, Greece
  *
  *     This software is provided under the terms described in
  *     the "machine/copyright.h" include file.
